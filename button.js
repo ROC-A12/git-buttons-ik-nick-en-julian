b58ea5a400b585941f517a85b3a4039db5cf0f35
NajikijaN <159284180+NajikijaN@users.noreply.github.com>
2025-04-03 10:35:12 +0200

    Comments en message

--- button.js
+++ button.js
@@ -14,7 +14,7 @@ const getRandomInt = function(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
 
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
 const showRandomMessage = function() {
